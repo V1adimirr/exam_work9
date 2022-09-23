ad482e3ea166e404a663e090987053221e7f1c5b
Vladimir <vladimirpedenko936@gmail.com>
2022-09-24 01:51:18 +0600

    создал новый проект. добавил приложения webapp и accounts

---
+++ source/accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+
+app_name = "accounts"
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name="login.html"), name="login"),
+    path('logout/', LogoutView.as_view(), name="logout"),
+    path('registration/', RegisterView.as_view(), name="registration"),
+    path('<int:pk>/', ProfileView.as_view(), name="profile"),
+]
